50ba98c92d8bd4724f1068e09bcbf4c24ba1c6c4
Salim <salim.artsider@gmail.com>
2025-04-10 08:59:06 +0100

    feat: update RootLayout to include NextTopLoader and modify metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,9 @@
 import type { Metadata } from 'next'
+import NextTopLoader from 'nextjs-toploader';
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'nest-stock',
 }
 
 export default function RootLayout({
@@ -14,7 +13,24 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body>{children}</body>
+      <body>
+        <NextTopLoader
+          color="#2299DD"
+          initialPosition={0.08}
+          crawlSpeed={200}
+          height={3}
+          crawl={true}
+          showSpinner={true}
+          easing="ease"
+          speed={200}
+          shadow="0 0 10px #2299DD,0 0 5px #2299DD"
+          template='<div class="bar" role="bar"><div class="peg"></div></div> 
+  <div class="spinner" role="spinner"><div class="spinner-icon"></div></div>'
+          zIndex={1600}
+          showAtBottom={false}
+        />
+        {children}
+      </body>
     </html>
   )
 }
