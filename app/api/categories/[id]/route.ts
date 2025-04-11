b45bfcbe37e12b32b98dd401e1241d9c8681caad
Salim <salim.artsider@gmail.com>
2025-04-11 11:49:13 +0100

    fix: update parameter destructuring in PUT and DELETE methods for consistency

--- app/api/categories/[id]/route.ts
+++ app/api/categories/[id]/route.ts
@@ -1,9 +1,9 @@
 import { NextResponse } from 'next/server';
 import { prisma } from '@/lib/prisma';
 
-export async function PUT(request: Request, { params }: { params: { id: string } }) {
+export async function PUT(request: Request, context: { params: { id: string } }) {
     try {
-        const { id } = params;
+        const { id } = context.params;
         const data = await request.json();
 
         const updatedCategory = await prisma.category.update({
@@ -20,16 +20,15 @@ export async function PUT(request: Request, { params }: { params: { id: string }
     }
 }
 
-export async function DELETE(request: Request, { params }: { params: { id: string } }) {
+export async function DELETE(request: Request, context: { params: { id: string } }) {
     try {
-        const { id } = params; // Pas besoin d'attendre avec await pour params
+        const { id } = context.params;
 
-        // Suppression du produit dans la base de données
         const deletedProduct = await prisma.category.delete({
             where: { id: parseInt(id) },
         });
 
-        return NextResponse.json(deletedProduct); // Retourne les données du produit supprimé
+        return NextResponse.json(deletedProduct);
     } catch (error) {
         return NextResponse.json(
             { error: (error as Error).message || "Une erreur est survenue" },
